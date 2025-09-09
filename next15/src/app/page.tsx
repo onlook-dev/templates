f2e71a4e0e13df28caefc78abc6a78a4286eaaca
Kiet Ho <hoakiet98@gmail.com>
2025-09-08 19:02:25 -0700

    make template dark mode

--- next15/src/app/page.tsx
+++ next15/src/app/page.tsx
@@ -2,12 +2,12 @@
 
 export default function Page() {
     return (
-        <div className="w-full min-h-screen flex items-center justify-center bg-white transition-colors duration-200 flex-col p-4 gap-4">
-            <div className="text-center text-gray-900 p-4">
+        <div className="w-full min-h-screen flex items-center justify-center bg-black transition-colors duration-200 flex-col p-4 gap-4">
+            <div className="text-center text-white p-4">
                 <h1 className="text-4xl font-semibold mb-4 tracking-tight">
                     Welcome to your app
                 </h1>
-                <p className="text-xl text-gray-800">
+                <p className="text-xl text-white">
                     This is a blank Onlook app with Next.js and TailwindCSS
                 </p>
             </div>
