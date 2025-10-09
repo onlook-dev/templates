21144d1a278460e55166ba02f3ec6dca480b3b41
Kiet Ho <hoakiet98@gmail.com>
2025-10-09 11:25:43 -0700

    upgrade next

--- next15/src/app/page.tsx
+++ next15/src/app/page.tsx
@@ -1,13 +1,19 @@
-'use client';
+"use client";
 
 export default function Page() {
     return (
-        <div className="w-full min-h-screen flex items-center justify-center bg-black transition-colors duration-200 flex-col p-4 gap-4">
-            <div className="text-center text-white p-4">
-                <h1 className="text-4xl font-semibold mb-4 tracking-tight">
+        <div
+            className="w-full min-h-screen flex items-center justify-center bg-white transition-colors duration-200 flex-col p-4 gap-4"
+            data-oid="in61k2."
+        >
+            <div className="text-center text-gray-900 p-4" data-oid="frh5au0">
+                <h1
+                    className="text-4xl font-semibold mb-4 tracking-tight"
+                    data-oid="snei1:i"
+                >
                     Welcome to your app
                 </h1>
-                <p className="text-xl text-white">
+                <p className="text-xl text-gray-800" data-oid="390bmag">
                     This is a blank Onlook app with Next.js and TailwindCSS
                 </p>
             </div>
